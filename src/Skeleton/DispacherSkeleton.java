dc8692944d32173f4d12964b40b8c9d12d5ca355
Marcellov97 <magiu2012@me.com>
2022-05-02 11:07:18 +0200

    Aggornamento runSkeleton - Skeleton

--- src/Skeleton/DispacherSkeleton.java
+++ src/Skeleton/DispacherSkeleton.java
@@ -19,6 +19,23 @@ public class DispacherSkeleton implements Dispacher {
 	
 	public void runSkeleton() {
 		// TODO metodo runSkeleton
+		implementazione = new DispacherImpl();
+		
+		try {
+			byte[] buffer = new byte[100];
+			DatagramPacket pacchetto = new DatagramPacket(buffer, buffer.length);
+			
+			socket.receive(pacchetto);
+			//estraggo informazioni dal pacchetto
+			InetAddress addr = pacchetto.getAddress();
+			int port = pacchetto.getPort();
+			int lenght = pacchetto.getLength();
+			byte[] data = pacchetto.getData();
+			// TODO continuare metodo
+		}
+		catch(Exception ex) {
+			ex.getStackTrace();
+		}
 	}
 	
 
