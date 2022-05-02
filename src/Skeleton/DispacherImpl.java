b79297509b3515a7779c7d41b99b6234eebe9eec
Marcellov97 <magiu2012@me.com>
2022-05-02 10:51:33 +0200

    Creazione Implementazione Server

---
+++ src/Skeleton/DispacherImpl.java
@@ -0,0 +1,24 @@
+package Skeleton;
+
+import Interface.Dispacher;
+
+public class DispacherImpl implements Dispacher {
+	
+	public DispacherImpl() {
+		
+	}
+
+	@Override
+	public void sendCmd(int command) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	@Override
+	public int getCmd() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
